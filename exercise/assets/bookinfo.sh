c6eed3ecae94908ec07b1d3a39a75a8111c3b382
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-10-02 15:32:36 +0300

    wait

--- exercise/assets/bookinfo.sh
+++ exercise/assets/bookinfo.sh
@@ -23,14 +23,16 @@ cp /tmp/bookinfo-no-deployment.yaml $HOME/exercise/bookinfo.yaml
 
 cat $HOME/exercise/bookinfo.yaml | kubectl -n bookinfo apply -f -
 
+kubectl get pods -n bookinfo -o name | xargs -I {} kubectl wait --for=condition=Ready --timeout=120s -n istio-system {}
+echo "##### pods have been running #####"
+kubectl get pods -n bookinfo -o name
+
 cat $istio_root/samples/bookinfo/networking/bookinfo-gateway.yaml | kubectl -n bookinfo apply -f - 
 
 cat /tmp/destination-rule-all-mtls.yaml | kubectl -n bookinfo apply -f -
-#kubectl get destinationrules -n bookinfo -o json | jq '.items[].metadata.name'
-#"productpage"
-#"ratings"
-#"reviews"
-#
 
 echo "Ingress node port: $(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')"
 
+echo "#### APP is starting ####"
+kubectl get svc productpage -n bookinfo -o jsonpath={@.spec.clusterIP} | xargs -I {} curl -I -s -S -L --retry-connrefused --retry 100 --retry-delay 5 {}:9080
+echo "#### APP have been starting ####" 
