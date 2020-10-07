8176d2d4834439f55a5a86f18ef3f33f909934a5
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-10-07 11:47:08 +0300

    UX

--- exercise/assets/bookinfo.sh
+++ exercise/assets/bookinfo.sh
@@ -7,12 +7,12 @@ cat /tmp/destination-rule-all-mtls.yaml | kubectl -n bookinfo apply -f -
 echo "Ingress node port: $(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')"
 
 echo "#### APP is starting";
-for (( i=1; i<1000; i++ )); 
+for (( i=1; i<=1000; i++ ))
 do 
   echo -n "."; 
   sleep .2;
-  kubectl get svc productpage -n bookinfo -o jsonpath={@.spec.clusterIP} 2> /dev/null | xargs -I {} curl -I -s -S -L --retry-connrefused --retry 100 --retry-delay 5 {}:9080
-  if [ $? -eq 0 ]; 
+  kubectl get svc productpage -n bookinfo -o jsonpath={@.spec.clusterIP} 2>/dev/null | xargs -I {} curl -I -s -S -L --retry-connrefused --retry 100 --retry-delay 5 {}:9080
+  if [ $? -eq 0 ]
   then
     break
   fi
