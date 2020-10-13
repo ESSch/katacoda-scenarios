088b5e80d7d3282f9eebd80a69b6b04bfb8aba91
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-10-13 10:37:12 +0300

    wait to start kubernetes

--- exercise/foreground.sh
+++ exercise/foreground.sh
@@ -2,6 +2,15 @@
 
 source launch.sh
 
+for i in {1..100}
+do
+    kubectl get pods --all-namespaces | wc -l > /dev/null
+    if [ $? -eq 0 ]
+    then
+        continue
+    fi
+done 
+
 for i in "$(kubectl get pods -n kube-system -o json | jq -r '.items[].metadata.name')"; 
 do
     kubectl get pods $i -n kube-system | grep -v NAME | grep Running > /dev/null
