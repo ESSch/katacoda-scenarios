4d7e3d1b2c1bd48a9117ca55fe01818cd548d519
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-10-14 08:58:20 +0300

    job

--- exercise/assets/bookinfo.job.sh
+++ exercise/assets/bookinfo.job.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 kubectl create -n default configmap bookinfo --from-file=/usr/local/bin/bookinfo.sh
 kubectl create -n default configmap kube --from-file=${HOME}/.kube/config
-#kubectl create -n default configmap tmp --from-dir=/tmp # /tmp/bookinfo-no-deployment.yaml /tmp/destination-rule-all-mtls.yaml
-#kubectl create -n default configmap tmp --from-file=/istio-1.6.2/samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl create -n default configmap deployment --from-file=/tmp/bookinfo-no-deployment.yaml
+kubectl create -n default configmap mtls --from-file=/tmp/destination-rule-all-mtls.yaml
+#kubectl create -n default configmap tmp --from-file=/tmp # /tmp/bookinfo-no-deployment.yaml /tmp/destination-rule-all-mtls.yaml
+kubectl create -n default configmap gateway --from-file=/istio-1.6.2/samples/bookinfo/networking/bookinfo-gateway.yaml
 chmod a+rw /root/.kube/config
 kubectl apply -n default -f /usr/local/bin/bookinfo.yaml
 kubectl get job bookinfo -n default
