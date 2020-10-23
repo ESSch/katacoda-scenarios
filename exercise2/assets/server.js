cb1825739a6fa1b1cabdaf00054124c9dcc0cebb
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-10-23 16:48:34 +0300

    probe: add liveness

--- exercise2/assets/server.js
+++ exercise2/assets/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 app.get("/", function(req, res) {
-    // sleep 5
-    // if [ $(( ( RANDOM % 2 ) + 1 )) == 1 ]; then echo 'Hello' > front.html; fi;
+    // if [ $(( ( RANDOM % 10 ) + 1 )) == 1 ]; then sleep 10 > front.html;
+    // if [ $(( ( RANDOM % 2 ) + 1 )) == 1 ]; then gen.sh > front.html; fi; # эмуляция неработоспособности
     res.download(front.html);
 });
 app.get("/liveness", function(req, res) {
