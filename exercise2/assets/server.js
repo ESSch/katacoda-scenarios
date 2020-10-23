967b96391cf4a34269187266cafaedab48fc219a
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-10-23 17:06:19 +0300

    probe: endpoint

--- exercise2/assets/server.js
+++ exercise2/assets/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 app.get("/", function(req, res) {
-    // if [ $(( ( RANDOM % 10 ) + 1 )) == 1 ]; then sleep 10 > front.html;
+    // if [ $(( ( RANDOM % 10 ) + 1 )) == 1 ]; then sleep 10; echo besy > status.txt;
     // if [ $(( ( RANDOM % 2 ) + 1 )) == 1 ]; then gen.sh > front.html; fi; # эмуляция неработоспособности
     res.download(front.html);
 });
-app.get("/liveness", function(req, res) {
-    let status = 200;
-    res.status(status);
-    res.send("Ok");
-});
-app.get("/readness", function(req, res) {
-    res.status(200);
-    res.send("Ok");
-});
+// app.get("/liveness", function(req, res) {
+//     const status = 200;
+//     res.status(status);
+//     res.send("Ok");
+// });
+// app.get("/readness", function(req, res) {
+//     const status = 200;
+//     res.status(status);
+//     res.send("Ok");
+// });
