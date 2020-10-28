7abbcf589b3f33a2726459c55edc35e85d0efa54
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-10-28 17:50:52 +0300

    a big container: get url

--- exercise2/assets/server.js
+++ exercise2/assets/server.js
@@ -1,28 +1,33 @@
 //     // if [ $(( ( RANDOM % 10 ) + 1 )) == 1 ]; then sleep 10; echo besy > status.txt;
 //     // if [ $(( ( RANDOM % 2 ) + 1 )) == 1 ]; then gen.sh > front.html; fi; # эмуляция неработоспособности
-
-
+const fs   = require("fs");
 const http = require("http");
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "text/html");
-    let status = 200; 
-    switch (req.url) {
-        case "/":
-            console.log({front: status});
-            const data = fs.readFileSync("front.html");
-            // res.writeHead(status);
-            res.end("Hello!");
-            break
-        case "/liveness":
-            console.log({liveness: status});
-            res.writeHead(status);
-            res.end(`${status}`);
-            break
-        case "/readiness":
-            console.log({readiness: status});
-            res.writeHead(status);
-            res.end(`${status}`);
-            break
+    try {
+        let status = 200; 
+        switch (req.url) {
+            case "/":
+                console.log({front: status});
+                const data = fs.readFileSync("front.html");
+                // res.writeHead(status);
+                res.end("Hello!");
+                break
+            case "/liveness":
+                console.log({liveness: status});
+                res.writeHead(status);
+                res.end(`${status}`);
+                break
+            case "/readiness":
+                console.log({readiness: status});
+                res.writeHead(status);
+                res.end(`${status}`);
+                break
+        }
+    } catch (err) {
+        console.log({err});
+        res.writeHead(500);
+        res.end(`${err}`);
     }
 };
 const server = http.createServer(requestListener);
