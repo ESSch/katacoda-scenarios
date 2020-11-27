7ee9adda04d1f91dcd61d7e9f125ad6f3f648bcf
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-11-27 16:36:58 +0300

    step 2 - update

--- exercise2/assets/update.sh
+++ exercise2/assets/update.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 sudo snap install yq
-cat app.yaml       | yq w - 'spec.template.spec.containers[0].volumeMounts[0].mountPath' /tmp > /tmp/app2.yaml
-cat /tmp/app2.yaml | yq w - 'spec.template.spec.containers[0].command' '["bash", "-c", "cp /tmp/* /app && node /app/server.js"]' > /tmp/app3.yaml
-cp /tmp/app3.yaml app.yaml
+cat /root/exercise/app.yaml | yq w - 'spec.template.spec.containers[0].volumeMounts[0].mountPath' /tmp > /tmp/app2.yaml
+cat /tmp/app2.yaml          | yq w - 'spec.template.spec.containers[0].command' '["bash", "-c", "cp /tmp/* /app && node /app/server.js"]' > /tmp/app3.yaml
+cp /tmp/app3.yaml /root/exercise/app.yaml
