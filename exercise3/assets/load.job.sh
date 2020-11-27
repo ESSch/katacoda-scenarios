69d4e9dcad2f3e56309bf5fa9a09864255064f1c
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-11-27 15:44:41 +0300

    step 3 - clear trash

--- exercise3/assets/load.job.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-kubectl create -n bookinfo configmap bookinfo --from-file=/usr/local/bin/load.sh
-kubectl create -n bookinfo configmap kube --from-file=${HOME}/.kube/config
-kubectl apply -n bookinfo -f /usr/local/bin/load.yaml
