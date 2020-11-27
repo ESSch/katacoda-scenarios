a672900db8d9e4b5697af91d88326dc843acd533
Штольц Евгений Сергеевич <ESShtolts@sberbank.ru>
2020-11-27 11:21:14 +0300

    stem2 - break

--- exercise2/assets/update.sh
+++ exercise2/assets/update.sh
@@ -1,4 +1,5 @@
-        volumeMounts:
-        - name: app
-          mountPath: /tmp
-        command: ["bash", "-c", "cp /tmp/* /app && node /app/server.js"]
+#! /bin/bash
+sudo snap install yq
+cat app.yaml       | yq w - 'spec.template.spec.containers[0].volumeMounts[0].mountPath' /tmp > /tmp/app2.yaml
+cat /tmp/app2.yaml | yq w - 'spec.template.spec.containers[0].command' '["bash", "-c", "cp /tmp/* /app && node /app/server.js"]' > /tmp/app3.yaml
+cp /tmp/app3.yaml app.yaml
